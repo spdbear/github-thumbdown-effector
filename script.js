16a6045b96c05a1ac17834d8b27dd844415798ff
spdbear <16793732+spdbear@users.noreply.github.com>
2020-08-29 23:34:05 +0900

    多重処理可能にした

--- script.js
+++ script.js
@@ -15,13 +15,15 @@
       console.log(v)
       buttons.forEach((btn) => {
         const animes = []
-        if (v.isSpinEnabled)
-          animes.push(
-            "animation: spin 0.25s cubic-bezier(0, 0.5, 0.5, 1) infinite"
-          )
-        if (v.isWiggleEnabled)
-          animes.push("wiggle 0.4s cubic-bezier(0, 0.5, 0.5, 1) infinite")
         if (v.isRainbowEnabled) animes.push("rainbow 1s linear infinite")
+        if (v.isWiggleEnabled && v.isRainbowEnabled) {
+          animes.push("spin-wiggle 0.35s cubic-bezier(0, 0.5, 0.5, 1) infinite")
+        } else {
+          if (v.isSpinEnabled)
+            animes.push("spin 0.25s cubic-bezier(0, 0.5, 0.5, 1) infinite")
+          if (v.isWiggleEnabled)
+            animes.push("wiggle 0.4s cubic-bezier(0, 0.5, 0.5, 1) infinite")
+        }
         if (v.isThinkingEnabled) btn.textContent = "🤔"
         btn.style.animation = animes.join(",")
       })
